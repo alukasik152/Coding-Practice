fa44d9708094246805baf965d845174f4dd9f8b1
alukasik152 <alukasik152@gmail.com>
2021-06-12 13:52:30 -0700

    replaced map with object

--- src/dup_values.ts
+++ src/dup_values.ts
@@ -1,31 +1,29 @@
-// https://www.javatpoint.com/typescript-map map functions
+import { transpileModule } from "typescript";
+
 
 //count # of duplicate values in a string
-function StringtoMap(input: string){
-    var word_array: string[] = input.split('');
-    let StringMap = new Map;
-    for(let i: number = 0; i < word_array.length; i++){
-        if(!StringMap.has(word_array[i]))
-            StringMap.set(word_array[i], 1);
-        else {
-            let count: number = StringMap.get(word_array[i]) + 1;
-            StringMap.delete(word_array[i]);
-            StringMap.set(word_array[i], count);
-        }    
-    }     
-    return StringMap;
-}
-
-function CountDup(input: string): number{
-    let inputMap = StringtoMap(input);
-    let dup: number = 0;
-    for(let MapValues of inputMap.values()){
-        if(MapValues > 1){
-            dup += MapValues;
-        }
+interface stringObj{
+    [key: string]: boolean;
+} 
+function stringToObj(input: string){
+    let inputObj: stringObj = {};
+    let stringArray: string[] = input.split('');
+    for(let i: number = 0; i < stringArray.length; i++){
+        if(stringArray[i] in inputObj)
+            inputObj[stringArray[i]] = true;
+        else
+            inputObj[stringArray[i]] = false;
     }
-    return dup;
+    return inputObj;
 }
 
-console.log("Antwon Lebowski", CountDup("Antwon Lebowski"));
-
+function countDup(inputObj: stringObj): number{
+    let count = 0;
+    let countObj = inputObj;
+    for (let value of Object.values(inputObj))
+        if (value === true)
+            count++;
+    return count;
+}
+let testObj = stringToObj("Josh and Bilbo drink from the same bowl");
+console.log(countDup(testObj));
