18fef4836aad243d686a12dce86602a8a2af20c3
alukasik152 <alukasik152@gmail.com>
2021-06-12 14:28:13 -0700

    change variable names

--- src/dup_values.ts
+++ src/dup_values.ts
@@ -2,22 +2,22 @@ import { transpileModule } from "typescript";
 
 
 //count # of duplicate values in a string
-interface stringObj{
+interface stringToObj{
     [key: string]: boolean;
 } 
 function stringToObj(input: string){
-    let inputObj: stringObj = {};
+    let dupTracker: stringToObj = {};
     let stringArray: string[] = input.split('');
     for(let i: number = 0; i < stringArray.length; i++){
-        if(stringArray[i] in inputObj)
-            inputObj[stringArray[i]] = true;
+        if(stringArray[i] in dupTracker)
+            dupTracker[stringArray[i]] = true;
         else
-            inputObj[stringArray[i]] = false;
+            dupTracker[stringArray[i]] = false;
     }
-    return inputObj;
+    return dupTracker;
 }
 
-function countDup(inputObj: stringObj): number{
+function countDup(inputObj: stringToObj): number{
     let count = 0;
     let countObj = inputObj;
     for (let value of Object.values(inputObj))
